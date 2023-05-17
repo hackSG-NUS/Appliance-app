6c4ee12fc24902719b94bbf3c053d4a2de41e37e
bob <leesengkittkitt@gmail.com>
2023-05-18 02:35:12 +0800

    added Firebase

---
+++ firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATcqL12qfXpFYwlb6flGAXqYm_NDJtxOU",
+  authDomain: "hack-singapore.firebaseapp.com",
+  projectId: "hack-singapore",
+  storageBucket: "hack-singapore.appspot.com",
+  messagingSenderId: "523422700598",
+  appId: "1:523422700598:web:5462ce66624b9f952e3497",
+  measurementId: "G-M2FHQ1YJH1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
